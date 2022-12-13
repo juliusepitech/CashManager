488a7857113cedb95057ed77a16e976cae569058
juliusepitech <julius-gauthier.kotannou@epitech.education>
2022-12-13 02:19:43 +0100

    changes

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ client.connect()
 app.get('/articles', (req, res)=>{
     try{
         client.query(`Select * from article`, (err, result)=>{
-            if(!err){
-                res.send(result.rows);
-            }
+            
+            res.send(result.rows);
+            
         });
         client.end;
     }catch(e){
@@ -26,9 +26,9 @@ app.get('/articles/:bardecode', (req, res)=>{
     try{
         client.query(`Select * from article where barecode='${req.params.bardecode}'`, (err, result)=>{
         
-            if(!err){
-                res.send(result.rows);
-            }
+            
+            res.send(result.rows);
+            
         });
         client.end;
     }catch(e){
